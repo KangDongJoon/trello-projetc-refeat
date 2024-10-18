d4d970dfbe200b9865cf1f011b2cb56b21a8c503
KangDongJoon <ehdwns1119@naver.com>
2024-10-19 00:37:14 +0900

    git init

---
+++ src/main/java/com/dj/trelloprojetcrepeat/TrelloProjetcRepeatApplication.java
@@ -0,0 +1,13 @@
+package com.dj.trelloprojetcrepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloProjetcRepeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloProjetcRepeatApplication.class, args);
+    }
+
+}
